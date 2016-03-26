9e964304384a453e0d67e6430690a5cfacc1db13
Checkwhei Sin <Checkwhei Sin>
2016-03-26 17:18:54 -0400

    successful retrieve Jeff Dean

--- SeleniumTest/src/main/src/WikiTest.java
+++ SeleniumTest/src/main/src/WikiTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ public class WikiTest {
 
     private static final String URL = "https://www.linkedin.com/";
 
-    private static final String keywordToSearch = "Micheal Sean";
+    private static final String keywordToSearch = "Jeff Dean Google";
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
@@ -37,7 +38,7 @@ public class WikiTest {
         driver.findElement(By.name("submit")).click();
 
 
-        System.out.println(driver.getPageSource());
+//        System.out.println(driver.getPageSource());
 
         driver.findElement(By.cssSelector("li[data-li-activity-type='addconnections'] > :first-child")).click();
 
@@ -50,10 +51,32 @@ public class WikiTest {
         List<WebElement> allPeople = driver.findElements(By.cssSelector(".title.main-headline"));
         /* use find elements here */
 
-        allPeople.get(0).click();
+        /* example 0 here */
+        WebElement zeroPerson = allPeople.get(0);
+        zeroPerson.click();
 
 
-        Thread.sleep(5000);
+        WebElement skillSection = driver.findElement(By.className("skills-section"));
+        List<WebElement> skillList = skillSection.findElements(By.tagName("li"));
+
+        for (WebElement webElement : skillList) {
+            String text = webElement.getAttribute("data-endorsed-item-name");
+            if (text != null && !text.equals("")) {
+                System.out.println("\t" + text.trim());
+
+                // create template methods to
+
+            }
+        }
+
+
+//        List<WebElement> skillList = driver.findElements(By.cssSelector("li [data-endorsed-item-name]"));
+
+
+//        System.out.println("\n\n\nskills = \t" + Arrays.toString(skillList.toArray()));
+
+
+
 
 //        driver.quit();
 
